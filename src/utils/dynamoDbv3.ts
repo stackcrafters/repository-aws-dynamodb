cbaf43ff5ed287506f712d003a25ef71b9cb7150
Peter Bouquet <pete@stackcrafters.com>
2023-06-01 14:30:04 +0100

    Remove undefined values by default

--- src/utils/dynamoDbv3.ts
+++ src/utils/dynamoDbv3.ts
@@ -2,4 +2,6 @@ import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 
 const client = new DynamoDBClient({});
-export const dbClient = DynamoDBDocumentClient.from(client);
+export const dbClient = DynamoDBDocumentClient.from(client, {
+  marshallOptions: { removeUndefinedValues: true }
+});
